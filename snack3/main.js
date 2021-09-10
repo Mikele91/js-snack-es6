bf9e29c6201333e39be2add6f3eeb3e4510d3676
Mikele91 <michele.amoruso91.8@gmail.com>
2021-09-10 16:31:04 +0200

    snack 3

---
+++ snack3/main.js
@@ -0,0 +1,50 @@
+// Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
+// La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
+// Usiamo i nuovi metodi degli array foreach o filter.
+
+
+const lista= [1, 2, 3, 4, 5, 6, 7];
+function myfunction(list, a, b){
+    
+    
+    let min = 0;
+    let max = 0;
+    if(a<= b){
+        min= a;
+        max =b
+    }else{
+        min= b;
+        max =a;
+    }
+    
+    
+    
+    while(max > list.length -1) {
+        console.log("hai inserito un numero troppo grande");
+        max = prompt("inserisci un nuovo numero valido");
+    } 
+
+    let newList= lista.filter(
+        (elem, i)=> {
+            if(i>=min && i <max){
+                return true
+            }
+        }
+        )
+        console.log(newList);
+
+    // newList =(lista.filter((elem, i) => i >= min&& i < max));
+
+
+//     // for(var i = min; i < max ; i++){
+//     //     newList.push(list[i]);
+        
+//     // }
+
+//     console.log(newList);
+//     return newList;
+}
+console.log(myfunction(lista, 1, 3));
+
+
+
